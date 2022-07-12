5c20579e2f407fcef7e92f082541d30fc3acc2b5
alibei <sebastiankotzias83@gmail.com>
2022-07-12 16:22:56 +0200

    sm6150-common: overlay: Remove  unused entries
    * Rename to cipher while at it

--- sm6150.mk
+++ sm6150.mk
@@ -266,7 +266,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-cipher
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
